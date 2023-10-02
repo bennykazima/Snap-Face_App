d05e45b350d10247b5f04e440c724de0faaa82c4
barantwenza <kazimabenny@.gmail.com>
2023-10-02 23:08:22 +0200

    the use of ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,33 +7,33 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  mySnap!: FaceSnap
-  myOtherSnap!: FaceSnap
-  myLastSnap!: FaceSnap
+  faceSnaps!: FaceSnap[]
   
   ngOnInit(){
-    this.mySnap= {
-      title: 'Burundi',
-      description: 'Le beau pays du burundi',
-      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      createdDate: new Date(),
-      snaps: 0,
-      location: 'Bujumbura',
-    };
-    this.myOtherSnap = {
-      title: 'Three Rock Mountain',
-      description: 'Un endroit magnifique pour les randonnées.',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-      createdDate: new Date(),
-      snaps: 0,
-      location: 'Kigali'
-    };
-    this.myLastSnap = {
-      title: 'Un bon repas',
-      description: 'Mmmh que c\'est bon !',
-      imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-      createdDate: new Date(),
-      snaps: 0
-    };
+    this.faceSnaps= [
+      {
+        title: 'Burundi',
+        description: 'Le beau pays du burundi',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        snaps: 200,
+        location: 'Bujumbura',
+      },
+      {
+        title: 'Three Rock Mountain',
+        description: 'Un endroit magnifique pour les randonnées.',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        location: 'Kigali'
+      },
+      {
+        title: 'Un bon repas',
+        description: 'Mmmh que c\'est bon !',
+        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+        createdDate: new Date(),
+        snaps: 0
+      }
+    ];
   }
 }
