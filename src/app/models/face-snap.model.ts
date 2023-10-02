3b69a9b056a300b8beaa8c5ee81299834bdfe6fb
barantwenza <kazimabenny@.gmail.com>
2023-10-02 20:43:30 +0200

    show model'elements in the app component

--- src/app/models/face-snap.model.ts
+++ src/app/models/face-snap.model.ts
@@ -1,7 +1,7 @@
 export class FaceSnap{
     constructor(public title: string, public description: string,
                 public imageUrl: string, public createDate: Date,
-                public createdDate: Date, public snaps: number){
+                public snaps: number){
         
     }
 }
