c4572c36a5a02ce1f14f71b34740180e8f7ff5bc
barantwenza <kazimabenny@.gmail.com>
2023-10-02 16:03:09 +0200

    add button in the template and its method in the TypeScript's file

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit{
+  title!: string;
+  description!: string;
+  createDate!: Date;
+  snaps!: number;
+  imageUrl!:string;
 
+  ngOnInit(){
+    this.title = 'Burundi';
+    this.description = 'Le beau pays du burundi';
+    this.createDate = new Date();
+    this.snaps = 6;
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
+  }
+
+  onAddSnap(){
+    this.snaps++;
+  }
 }
